614cad247f39f3bd80cf76802831526a43834632
Kizmet <bryantpatton@gmail.com>
2019-05-13 16:25:09 -0700

    fixed h1

--- js/index.js
+++ js/index.js
@@ -1,42 +1,3 @@
-// const siteContent = {
-//   "nav": {
-//     "nav-item-1": "Services",
-//     "nav-item-2": "Product",
-//     "nav-item-3": "Vision",
-//     "nav-item-4": "Features",
-//     "nav-item-5": "About",
-//     "nav-item-6": "Contact",
-//     "img-src": "img/logo.png"
-//   },
-//   "cta": {
-//     "h1": "DOM Is Awesome",
-//     "button": "Get Started",
-//     "img-src": "img/header-img.png"
-//   },
-//   "main-content": {
-//     "features-h4":"Features",
-//     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//     "about-h4":"About",
-//     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//     "middle-img-src": "img/mid-page-accent.jpg",
-//     "services-h4":"Services",
-//     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//     "product-h4":"Product",
-//     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//     "vision-h4":"Vision",
-//     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   },
-//   "contact": {
-//     "contact-h4" : "Contact",
-//     "address" : "123 Way 456 Street Somewhere, USA",
-//     "phone" : "1 (888) 888-8888",
-//     "email" : "sales@greatidea.io",
-//   },
-//   "footer": {
-//     "copyright" : "Copyright Great Idea! 2018"
-//   },
-// };
-
 import  {siteContent} from './siteContent.js'; // or './module'
 let val =siteContent;  // val is "Hello";
 
@@ -72,7 +33,20 @@ navsNew.forEach(element => {element.style.color = "green";});
 // let sections = document.getElementsByTagName("section");
 let ctaSection = document.querySelector(".cta");
 let ctaH1 = ctaSection.querySelector('h1')
-ctaH1.textContent = siteContent["cta"]["h1"];
+let h1List = siteContent["cta"]["h1"].split(' ');
+// ctaH1.textContent = siteContent["cta"]["h1"].split(' \r\n');
+ctaH1.textContent = h1List[0];
+let s= document.createElement("span");
+s.setAttribute('style', "display: block");
+s.textContent = h1List[1];
+ctaH1.append(s)
+
+let s1= document.createElement("span");
+s1.setAttribute('style', "display: block");
+s1.textContent = h1List[2];
+ctaH1.append(s1)
+
+
 let ctaButton = ctaSection.querySelector('button');
 ctaButton.textContent = siteContent["cta"]["button"];
 let ctaImg = ctaSection.querySelector('img');
